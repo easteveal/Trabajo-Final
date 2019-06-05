9ef687b663e535d228c58a00e710fa20c3d19fcd
easteveal <50872952+easteveal@users.noreply.github.com>
2019-06-05 09:02:55 -0500

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -14,11 +14,13 @@ t.remove(b)
 while True:
     print("MENU:")
     print("Opcion 1: Registrar datos")
-    print("Opcion 2: Mostrar resumen")
-    print("Opcion 3: BINGO!!!")
-    print("Opcion 4: Reiniciar juego")
-    print("Opcion 5: Finalizar juego")
-    print("Opción 6: Mostrar pozo ganado")
+    print("Opcion 2: Empezar a jugar")
+    print("Opcion 3: Siguiente giro")
+    print("Opcion 4: Mostrar resumen")
+    print("Opcion 5: BINGO!!!")
+    print("Opcion 6: Reiniciar juego")
+    print("Opcion 7: Finalizar juego")
+    print("Opción 8: Mostrar pozo ganado")
     op = int(input("Digite la opción que necesite: "))
     if 1<=op<=4:
         break
@@ -27,18 +29,27 @@ if op == 1:
     j1c = int(input("Ingrese el número de cartillas que posee: "))
     op = int(input("Digite la opción que necesite: "))
 elif op == 2:
+    print("Empezar el juego:")
+    for i in range(15):
+        print(random.randint(1,80)) 
+elif op == 3:
+    print("Siguiente giro:")
+    for i in range(15):
+        print(random.randint(1,80)) 
+
+elif op == 4:
     print(r)
     op = int(input("Digite la opción que necesite: "))
-elif op == 3:
+elif op == 5:
     print("Juego finalizado\nMostrando pozo ganado")
     op = int(input("Digite la opción que necesite: "))
-elif op == 4:
+elif op == 6:
     print("Reiniciando el juego...\n..que empiece el juego")
     op = int(input("Digite la opción que necesite: "))
-elif op == 5:
+elif op == 7:
     print("Se acabó el juego")
     op = int(input("Digite la opción que necesite: "))
-elif op == 6:
+elif op == 8:
     print("Mostrar el pozo")
     op = int(input("Digite la opción que necesite: "))
-    print("Avance del proyecto ... faltan cambios xd")
+    print("Avance del proyecto ... faltan cambios")
